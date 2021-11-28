8ab6c88e992608c09452508e24a96dfea8ef1d47
Narenthar <80049709+Narenthar@users.noreply.github.com>
2021-11-28 11:52:15 +0530

    organize more

--- routes/movies.js
+++ routes/movies.js
@@ -1,51 +1,56 @@
-
 import express from "express";
-import { getMovieById, deleteMoviesById, insertMovies, updateMoviesById, getMovies } from "../helper.js";
-
+import {
+  getMovieById,
+  deleteMoviesById,
+  insertMovies,
+  updateMoviesById,
+  getMovies,
+} from "../helper.js";
 
 const router = express.Router();
-router.get("/:id", async (request, response) => {
-    console.log(request.params)
+
+router
+  .route("/")
+  .get(async (request, response) => {
+    console.log(request.query);
+    let filter = request.query;
+    if (filter.rating) {
+      filter.rating = +filter.rating;
+    }
+    const filteredMovies = await getMovies(filter);
+    response.send(filteredMovies);
+  })
+  .post(async (request, response) => {
+    const data = request.body;
+    console.log(data);
+    const result = await insertMovies(data);
+    response.send(result);
+  });
+
+router
+  .route("/:id")
+  .get(async (request, response) => {
+    console.log(request.params);
     const { id } = request.params;
     const movie = await getMovieById(id);
     console.log(movie);
     const notFound = { message: "Movie not found" };
     movie ? response.send(movie) : response.status(404).send(notFound);
-  });
-  router.delete("/:id", async (request, response) => {
-    console.log(request.params)
+  })
+  .delete(async (request, response) => {
+    console.log(request.params);
     const { id } = request.params;
     const movie = await deleteMoviesById(id);
     console.log(movie);
     const notFound = { message: "Movie not found" };
     movie ? response.send(movie) : response.status(404).send(notFound);
-  });
-  
-  router.post("/", async (request, response) => {
-   
-    const data = request.body
-    console.log(data)
-    const result = await insertMovies(data);
-   response.send(result)  
-  });
-  
-  router.put("/:id", async (request, response) => {
-   const {id} = request.params
-    const data = request.body
-    console.log(data)
+  })
+  .put(async (request, response) => {
+    const { id } = request.params;
+    const data = request.body;
+    console.log(data);
     const result = await updateMoviesById(id, data);
-   response.send(result)  
-  });  
-  
-  
-  router.get("/",async (request,response)=>{
-      console.log(request.query)
-      let filter = request.query;
-      if(filter.rating){
-        filter.rating= +filter.rating
-      }
-      const filteredMovies = await getMovies(filter);
-   response.send(filteredMovies) 
+    response.send(result);
   });
 
-  export const moviesRouter = router;
+export const moviesRouter = router;
